c4b2a18b8b6e432bde43ec883feaaa4fbb65c076
IgorSilvaZZ <igor06silva12@gmail.com>
2022-08-19 18:59:14 -0300

    Add problem 1133

---
+++ Iniciante/Module3/restoDivisao/script.js
@@ -0,0 +1,14 @@
+const { readFileSync } = require("fs");
+
+const [numberOne, numberTwo] = readFileSync("stdin", "utf-8")
+  .split("\n")
+  .map((item) => parseInt(item));
+
+let initial = numberTwo < numberOne ? numberTwo : numberOne;
+let end = numberOne > numberTwo ? numberOne : numberTwo;
+
+for (let i = initial; i < end; i++) {
+  if (i % 5 === 2 || i % 5 === 3) {
+    console.log(i);
+  }
+}
