2bbc685aee90dd44c874afed030555bd6825bbaf
IgorSilvaZZ <igor06silva12@gmail.com>
2022-09-12 19:24:42 -0300

    Accepted problem 1183

--- Iniciante/Module5/acimaDiagonalPrincipal/script.js
+++ Iniciante/Module5/acimaDiagonalPrincipal/script.js
@@ -2,46 +2,40 @@ const { readFileSync } = require("fs");
 
 const lines = readFileSync("stdin", "utf-8").split("\n");
 
-const [operation] = lines;
+let [operation] = lines;
 
-const matriz = [];
+operation = operation.trim();
 
-let indexValue = 1;
+const matrix = [];
+
+let indexLine = 1;
 
 let some = 0;
 
 for (let line = 0; line <= 11; line++) {
   const column = [];
 
-  for (let col = 0; col <= 11; col++, indexValue++) {
-    column.push(Number(lines[indexValue]));
+  for (let col = 0; col <= 11; col++, indexLine++) {
+    column.push(Number(lines[indexLine]));
   }
 
-  matriz.push(column);
+  matrix.push(column);
 }
 
-if (operation.trim() == "S") {
-  for (let line = 0; line < 11; line++) {
-    for (let col = 0; col < 11; col++) {
-      if (col > line) {
-        some += matriz[line][col];
-      }
+for (let line = 0; line <= 11; line++) {
+  for (let col = 0; col <= 11; col++) {
+    if (col > line) {
+      some += matrix[line][col];
     }
   }
+}
 
+if (operation == "S") {
   console.log(some.toFixed(1));
 }
 
-if (operation.trim() == "M") {
-  for (let line = 0; line < 11; line++) {
-    for (let col = 0; col < 11; col++) {
-      if (col > line) {
-        some += matriz[line][col];
-      }
-    }
-  }
+if (operation == "M") {
+  some /= (matrix.length * matrix.length - 12) / 2;
 
-  const arrange = some / (matriz.length * matriz.length - 12) / 2;
-
-  console.log(arrange.toFixed(1));
+  console.log(some.toFixed(1));
 }
