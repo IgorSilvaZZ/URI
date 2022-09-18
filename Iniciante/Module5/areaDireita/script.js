88e5062fb9a8d7a9305a44a3dd06d4a07f430aca
IgorSilvaZZ <igor06silva12@gmail.com>
2022-09-18 11:32:24 -0300

    Add problem 1190

---
+++ Iniciante/Module5/areaDireita/script.js
@@ -0,0 +1,42 @@
+const { readFileSync } = require("fs");
+
+const lines = readFileSync("stdin", "utf-8").split("\n");
+
+let [operation] = lines;
+
+operation = operation.trim();
+
+const matrix = [];
+
+let indexMatrix = 1;
+let some = 0;
+let totalElements = 0;
+
+for (let line = 0; line <= 11; line++) {
+  const column = [];
+
+  for (let col = 0; col <= 11; col++, indexMatrix++) {
+    column.push(Number(lines[indexMatrix]));
+  }
+
+  matrix.push(column);
+}
+
+for (let line = 0; line <= 11; line++) {
+  for (let col = 11; col >= 0; col--) {
+    if (col > line && col > matrix.length - line - 1) {
+      some += matrix[line][col];
+      totalElements++;
+    }
+  }
+}
+
+if (operation == "S") {
+  console.log(some.toFixed(1));
+}
+
+if (operation == "M") {
+  some /= totalElements;
+
+  console.log(some.toFixed(1));
+}
