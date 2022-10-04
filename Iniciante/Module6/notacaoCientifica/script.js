b7f23a35df72be391f2887e5f099190ff0fada67
IgorSilvaZZ <igor06silva12@gmail.com>
2022-10-04 16:30:24 -0300

    Add problem 1958

--- Iniciante/Module6/notacaoCientifica/script.js
+++ Iniciante/Module6/notacaoCientifica/script.js
@@ -12,4 +12,17 @@ if (number > 0) {
   notationFormatter = notation.padStart(10, "-");
 }
 
-console.log(notationFormatter);
+let [notationTransform, numberForNotation] = notationFormatter.split("E");
+
+let numberForNotationFormatter = numberForNotation;
+
+if (numberForNotation.length < 3) {
+  const withoutMin = numberForNotation.slice(1);
+
+  numberForNotationFormatter =
+    Number(numberForNotation) < 0
+      ? "-" + withoutMin.padStart(2, "0")
+      : "+" + withoutMin.padStart(2, "0");
+}
+
+console.log(`${notationTransform}E${numberForNotationFormatter}`);
