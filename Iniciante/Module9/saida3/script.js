a0eea3937cd153ab7ac492880b7b68b7fc87c4bd
IgorSilvaZZ <igor06silva12@gmail.com>
2022-11-30 23:55:23 -0300

    Init problem 2749

---
+++ Iniciante/Module9/saida3/script.js
@@ -0,0 +1,27 @@
+const { readFileSync } = require("fs");
+
+const lines = readFileSync("stdin", "utf-8").split("\n");
+
+let screen = "";
+
+for (let line = 1; line <= 7; line++) {
+  for (let col = 1; col <= 39; col++) {
+    if (line === 1 || line === 7) {
+      screen += "-";
+    } else if (col === 1 || col === 39) {
+      screen += "|";
+    } else if (line === 2) {
+      if (col === 2) {
+        screen += "x = 35";
+      } else if (col < 2 || col > 7) {
+        screen += " ";
+      }
+    } else {
+      screen += " ";
+    }
+  }
+
+  screen += "\n";
+}
+
+console.log(screen.trim());
