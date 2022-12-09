4cdb05e9603782613f1c1f19efb6138ac5d2d29a
IgorSilvaZZ <igor06silva12@gmail.com>
2022-12-08 23:28:00 -0300

    Add problem 2750

--- Iniciante/Module9/saida4/script.js
+++ Iniciante/Module9/saida4/script.js
@@ -4,9 +4,9 @@ const lines = readFileSync("stdin", "utf-8").split("\n");
 
 let screen = "";
 
-for (let line = 1; line <= 7; line++) {
+for (let line = 1; line <= 20; line++) {
   for (let col = 1; col <= 39; col++) {
-    if (line === 1 || line === 7) {
+    if (line === 1 || line === 20 || line == 3) {
       screen += "-";
     } else if (col === 1 || col === 13 || col === 23 || col === 39) {
       screen += "|";
@@ -17,11 +17,24 @@ for (let line = 1; line <= 7; line++) {
         screen += "octal";
       } else if (col === 26) {
         screen += "Hexadecimal";
-      } else if (col < 4 || col > 20) {
+      } else if (
+        col < 4 ||
+        col > 36 ||
+        (col > 10 && col < 18) ||
+        (col > 21 && col < 26)
+      ) {
         screen += " ";
       }
     } else {
-      screen += " ";
+      if (col === 8) {
+        screen += `${line - 4}`;
+      } else if (col === 18) {
+        screen += `${(line - 4).toString(8)}`;
+      } else if (col === 31) {
+        screen += `${(line - 4).toString(16).toUpperCase()}`;
+      } else if (!(line > 11 && col === 14) && !(line > 13 && col === 2)) {
+        screen += " ";
+      }
     }
   }
 
