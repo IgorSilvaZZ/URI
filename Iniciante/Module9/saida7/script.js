07d27187c8cadc6a7f79cdc47e63535c794e12ad
IgorSilvaZZ <igor06silva12@gmail.com>
2023-04-02 22:09:32 -0300

    Add problem 2753

---
+++ Iniciante/Module9/saida7/script.js
@@ -0,0 +1,42 @@
+const { readFileSync } = require("fs");
+
+const lines = readFileSync("stdin", "utf-8").split("/n");
+
+let count = 97;
+
+const values = Array.from({ length: 26 }, (item, index) => {
+  return count + index;
+});
+
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+alphabet.forEach((item, index) => {
+  console.log(`${values[index]} é ${item}`);
+});
