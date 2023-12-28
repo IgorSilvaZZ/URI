c9db5621f581e326f076398f81914454cf8b16ea
IgorSilvaZZ <igor06silva12@gmail.com>
2023-12-27 21:03:05 -0300

    Refactor problem 1041

--- Iniciante/Module1/coordenadas/script.js
+++ Iniciante/Module1/coordenadas/script.js
@@ -1,15 +1,15 @@
 const { readFileSync } = require("fs");
 
-const linha = readFileSync("stdin", "utf-8");
+const lines = readFileSync("stdin", "utf-8");
 
-const [x, y] = linha.split(" ").map((item) => parseFloat(item).toFixed(1));
+const [x, y] = lines.split(" ").map((item) => parseFloat(item));
 
-if (x == 0.0 && y == 0.0) {
+if (x === 0.0 && y === 0.0) {
   console.log(`Origem`);
-} else if (x == 0.0) {
-  console.log(`Eixo X`);
-} else if (y == 0.0) {
+} else if (x === 0.0) {
   console.log(`Eixo Y`);
+} else if (y === 0.0) {
+  console.log(`Eixo X`);
 } else if (x > 0.0 && y > 0.0) {
   console.log(`Q1`);
 } else if (x < 0.0 && y > 0.0) {
