5e58966ac1c77d1afd674ad05946d45e58bd40ef
IgorSilvaZZ <igor06silva12@gmail.com>
2025-02-07 08:48:12 -0300

    refactor: :test_tube: Initial tests for problem

--- Iniciante/Module11/maisCavalos/script.js
+++ Iniciante/Module11/maisCavalos/script.js
@@ -27,12 +27,14 @@ for (let line = 7; line >= 0; line--) {
   board.push(column);
 }
 
-const [columnInitialPosition, lineInitialPosition] = initialPosition.split("");
+let [columnInitialPosition, lineInitialPosition] = initialPosition.split("");
+
+lineInitialPosition = Number(lineInitialPosition);
 
 const translateColumnInitialPosition = columnsBoardMap[columnInitialPosition];
 
 /* Setando a posição atual do cavalo */
-board[Number(lineInitialPosition)][translateColumnInitialPosition] = true;
+board[lineInitialPosition][translateColumnInitialPosition] = true;
 
 /* Movimentos Disponiveis */
 let validateMovements = 8;
@@ -40,7 +42,53 @@ let validateMovements = 8;
 /* Dividindo para que seja duas partes das colunas (Para tras e Para frente) */
 const availableTotalColumns = validateMovements / 2;
 
+const currentLine = board[lineInitialPosition];
+
 /* Indentificando as linhas disponiveis que vão em cada coluna */
 for (let i = 1; i <= availableTotalColumns; i++) {
-  let currentColumn = translateColumnInitialPosition + 1;
+  let currentColumn = translateColumnInitialPosition;
+
+  /* Deslocamento da coluna */
+  let displacementColumn = 0;
+
+  if (i <= 2) {
+    if (i === 1) {
+      displacementColumn = 1;
+      currentColumn -= displacementColumn;
+    } else if (i === 2) {
+      displacementColumn = 2;
+      currentColumn -= displacementColumn;
+    }
+  } else {
+    if (i === 3) {
+      displacementColumn = 1;
+      currentColumn += 1;
+    } else if (i === 4) {
+      displacementColumn = 2;
+      currentColumn += 2;
+    }
+  }
+
+  if (
+    currentLine[currentColumn] !== undefined &&
+    currentLine[currentColumn] === false
+  ) {
+    let displacementLine = 0;
+
+    /* Descolando duas linhas para cada coluna */
+    /* Ou seja serão 4 movimentos de deslocamento de linha para cada coluna */
+    if (displacementColumn === 1) {
+      displacementLine = 2;
+
+      board[lineInitialPosition - displacementLine][currentColumn] = true;
+      board[lineInitialPosition + displacementLine][currentColumn] = true;
+    } else {
+      displacementLine = 1;
+
+      board[lineInitialPosition - displacementLine][currentColumn] = true;
+      board[lineInitialPosition + displacementLine][currentColumn] = true;
+    }
+  }
 }
+
+console.table(board);
